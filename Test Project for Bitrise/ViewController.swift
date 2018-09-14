8a664b3a739816959825822807951e361b6fba89
Simon Tsai <simon.tsai.j@gmail.com>
2018-09-14 13:51:17 -0700

    Initial Commit

---
+++ Test Project for Bitrise/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test Project for Bitrise
+//
+//  Created by Simon Tsai on 9/14/18.
+//  Copyright © 2018 TribalScale. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
